8ed9f538b30c431401ddaa129bf3cb7d9a9e6349
Daniel Rocha <danrochamaia@gmail.com>
2024-01-31 02:25:41 -0300

    commit day 1 and day 2

---
+++ 7DaysOfCode/Entities/Pokemon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7DaysOfCode.Entities
+{
+    internal class Pokemon
+    {
+        public List<Ability> abilities {  get; set; }
+
+        public List<Form> forms {  get; set; }
+
+        public int height { get; set; }
+
+        public int weight { get; set; }
+    }
+}
---
+++ 7DaysOfCode/Program.cs
@@ -0,0 +1,49 @@
+﻿using _7DaysOfCode.Entities;
+using RestSharp;
+using System.Text.Json;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var pokemons = new Dictionary<int, string> { { 1, "pikachu" }, { 2, "bulbasaur" }, { 3, "charmander" }, { 4, "meowth" }, { 5, "zapdos" }, { 6, "mewtwo" }, { 0, "encerrar" } };
+
+        Console.WriteLine("Pokemons disponíveis para consulta: ");
+
+        foreach (var p in pokemons)
+        {
+            Console.WriteLine($"{p.Key} - {p.Value}");
+        }
+
+        while (true)
+        {
+            Console.Write("\nInsira o id do pokemon para verificar seus atributos: ");
+            var pokemonId = Console.ReadLine();
+            if(pokemonId == "0")
+            {
+                Console.WriteLine("Até mais!");
+                break;
+            }
+
+            var pokemonEscolhido = pokemons[int.Parse(pokemonId)];
+
+            if (pokemonEscolhido != default)
+            {
+                var client = new RestClient($"https://pokeapi.co/api/v2/pokemon/{pokemonEscolhido}");
+                var request = new RestRequest("", Method.Get);
+                var response = client.Execute(request);
+                var pokemon = JsonSerializer.Deserialize<Pokemon>(response.Content);
+
+                Console.WriteLine($"Nome pokemon: {pokemon.forms.FirstOrDefault().name}");
+                Console.WriteLine($"Altura: {pokemon.height}");
+                Console.WriteLine($"Peso: {pokemon.weight}");
+                Console.WriteLine($"Habilidades:");
+                pokemon.abilities.ForEach(a => Console.WriteLine(a.ability.name.ToUpper()));
+            }
+            else
+            {
+                Console.WriteLine("Id não encontrado.");
+            }
+        }
+    }
+}
